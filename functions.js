8ed3bf474f7f321e77bf71a20ce1c6555fd5dc70
moez mzeh <moez.mzah@gmail.com>
2021-04-19 11:41:30 +0100

    ws-ES6

---
+++ functions.js
@@ -0,0 +1,6 @@
+//named export
+export const sum = (a, b) => a + b;
+export const product = (a, b) => a * b;
+//export default
+const minus = (a, b) => a - b;
+export default minus;
---
+++ main.js
@@ -0,0 +1,6 @@
+import soustraction, { sum, product } from "./functions";
+// import * as all from './functions'
+// import soustraction from './functions'
+console.log(sum(2, 8));
+console.log(product(2, 8));
+console.log(soustraction(2, 1));
